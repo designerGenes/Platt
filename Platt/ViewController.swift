6d47ca1178eb74630f533802f097df59b1a289cd
Jaden Nation <jnationdesignerjeans@gmail.com>
2019-04-22 12:25:17 -0700

    Initial Commit

---
+++ Platt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Platt
+//
+//  Created by Jaden Nation on 4/22/19.
+//  Copyright © 2019 Designer Jeans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
